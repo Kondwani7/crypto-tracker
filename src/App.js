228d813d1aa4b9df3a98a2c26a3b162efaefa4e4
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2021-12-06 11:32:04 +0200

    getting data from the  api and rendering on the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from "react";
-
-import axios from  'axios'
-
-
+import axios from  'axios';
+import Coin from './modules/Coin';
 
 function App() {
-    const [coin, setCoin] = useState([]);
+    const [coins, setCoin] = useState([]);
+    const [search, setSearch] = useState('');
     
 
     useEffect(()=>{
@@ -15,14 +14,36 @@ function App() {
         })
         .catch(error => console.log('error'))
     })
+
+    const handleChange = e => {
+        setSearch(e.target.value)
+    }
+
+    const filteredCoins = coins.filter(coin => 
+        coin.name.toLowerCase().includes(search.toLowerCase())
+    )
   return (
     <div className="coin-app">
         <div className='coin-search'>
             <h3 className='coin-text'>Search for a currency</h3>
             <form>
-                <input className='coin-input' placeholder='search' type='text'/>
+                <input className='coin-input' placeholder='search' type='text'
+                    onchange={handleChange}
+                />
             </form>
         </div>
+        {filteredCoins.map(coin => {
+            return <Coin
+                key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                price={coin.current_price}
+                marketCap={coin.market_cap}
+                priceChange={coin.price_change_percentage_24h}
+                volume={coin.total_volume}
+            />
+        })}
     </div>
   );
 }
--- src/modules/Coin.js
+++ src/modules/Coin.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const Coin = () => {
+const Coin = ({name, image, symbol, price, marketCap,priceChange, volume }) => {
     return (
-        <div>
-            
+        <div className='coin-container'>
+            <div className='coin-row'>
+                <div className='coin'>
+                    <img src={image} alt='crypto'/>
+                    <h4>{name}</h4>
+                    <p className='coin-symbol'>{symbol}</p>
+                </div>
+                <div className='coin-data'>
+                    <p className='coin-price'>${price.toLocaleString()}</p>
+                    <p className='coin-marketCap'>${marketCap.toLocaleString()}</p>
+                    
+                    {priceChange < 0? (
+                        <p className='coin-percentage red'>{priceChange.toFixed(2)}%</p>
+                    ) :(
+                        <p className='coin-percentage green'>{priceChange.toFixed(2)}%</p>
+                    )}
+                    <p className='coin-volume'>{volume.toLocaleString()}</p>
+                </div>
+            </div>
         </div>
     )
 }
